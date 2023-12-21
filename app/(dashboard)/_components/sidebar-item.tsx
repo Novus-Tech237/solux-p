25061d499975bd8e12bd22c2197bda0c096a6aee
Novus-Tech237 <novustechnologies7@gmail.com>
2023-12-21 10:33:02 +0100

    Changes to be committed: 	new file:   app/(dashboard)/_components/mobile-sidebar.tsx 	new file:   app/(dashboard)/_components/navbar.tsx 	modified:   app/(dashboard)/_components/sidebar-item.tsx 	modified:   app/(dashboard)/layout.tsx 	new file:   app/(dashboard)/search/page.tsx 	new file:   components/ui/sheet.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,6 +1,7 @@
 "use client"
-
+import { cn } from "@/lib/utils"
 import { LucideIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
 
 interface SidebarItemProps{
     icon: LucideIcon;
@@ -8,10 +9,30 @@ interface SidebarItemProps{
     href: string;
 };
 
-export const SidebarItem = ({icon, label, href}:SidebarItemProps) => {
+export const SidebarItem = ({icon:Icon, label, href}:SidebarItemProps) => {
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const isActive = (pathname ==="/" && href === "/") || pathname === href || pathname?.startsWith(`${href}/`)
+
+    const onClick = () =>{router.push(href)}
+
     return (
-        <div>
-            Sidebar Item
+        <button
+         onClick={onClick} 
+         type="button"
+         className={ cn(
+            "flex items-center gap-x-2 text-slate-500 font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+            isActive && "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700")} 
+        >
+        <div className="flex items-center gap-x-2 py-4">
+            <Icon
+                size={22}
+                className={cn("text-slate-500", isActive && "text-sky-700")}
+            />
+            {label}
         </div>
+        <div className={cn("ml-auto opacity-0 border-2 border-sky-700 w-full transition-all", isActive && "opacity-100")}></div>
+        </button>
     )
 }   
