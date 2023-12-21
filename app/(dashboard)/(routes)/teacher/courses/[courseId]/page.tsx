f86b49a0f63711c74c55405f53c3d80561d4f71f
Novus-Tech237 <novustechnologies7@gmail.com>
2023-12-21 22:29:51 +0100

    Changes to be committed: 	new file:   app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx 	new file:   app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx 	modified:   app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx 	new file:   app/api/courses/[courseId]/route.ts 	new file:   components/ui/textarea.tsx

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,8 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({params}:{params:{courseId: string}}) => {
     const { userId } = auth();
@@ -40,6 +42,14 @@ const CourseIdPage = async ({params}:{params:{courseId: string}}) => {
                         <IconBadge icon={LayoutDashboard}/>
                         <h2 className="text-xl">Customize your Course</h2>
                     </div>
+                    <TitleForm
+                        initialData={course}
+                        courseId={course.id}
+                    /> 
+                    <DescriptionForm
+                        initialData={course}
+                        courseId={course.id}
+                    /> 
                 </div>
             </div>
         </div>
