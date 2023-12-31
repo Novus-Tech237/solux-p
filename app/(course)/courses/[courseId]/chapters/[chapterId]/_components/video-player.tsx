5e5d50e5fb58d8880a8dc06db2dac23d5fac361e
Novus-Tech237 <novustechnologies7@gmail.com>
2024-01-01 00:36:58 +0100

    Changes to be committed: 	new file:   actions/get-chapter.ts 	new file:   app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx 	modified:   app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx 	modified:   app/(course)/courses/[courseId]/page.tsx

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import axios from "axios"
+import MuxPlayer from "@mux/mux-player-react"
+import { useState } from "react"
+import { toast } from "react-hot-toast"
+import { useRouter } from "next/navigation"
+import { Loader2, Lock } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { useConfettiStore } from "@/hooks/use-confetti-store"
+
+interface VideoPlayerProps{
+    playbackId: string;
+    courses: string;
+    chapterId: string;
+    nextChapterId?: string;
+    isLocked: boolean;
+    completeOnEnd: boolean;
+    title: string;
+}
+export const VideoPlayer =({playbackId, courses, chapterId, nextChapterId, isLocked, completeOnEnd, title}: VideoPlayerProps) => {
+    return(
+        <div>
+            VideoPlayer
+        </div>
+    )
+}
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,8 +1,60 @@
-const ChapterIdPage = () => {
+import { getChapter } from "@/actions/get-chapter";
+import { Banner } from "@/components/banner";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({params}:{params:{courseId: string; chapterId: string}}) => {
+    const { userId } = auth();
+    if(!userId){
+        return redirect("/")
+    }
+    const {
+        chapter,
+        course,
+        muxData,
+        attachments,
+        nextChapter,
+        userProgress,
+        purchase,
+    } = await getChapter({
+        userId,
+        chapterId: params.chapterId,
+        courseId: params.courseId
+    })
+    if (!chapter || !course){
+        return redirect("/")
+    }
+    const isLocked = !chapter.isFree && !purchase;
+    const completeOnEnd = !!purchase && !userProgress?.isCompleted
     return ( 
         <div>
-            ChapterId
+             {userProgress?.isCompleted && (
+                <Banner
+                    variant="success"
+                    label="You have completed this chapter"
+                />
+             )}
+             {isLocked && (
+                <Banner
+                    variant="warning"
+                    label="You need to purchase this course to continue"
+                />
+             )}
+             <div className="flexnflex-col max-w-4xl mx-auto pb-20">
+                <div className="p-4">
+                    <VideoPlayer 
+                        chapterId={params.chapterId}
+                        title={chapter.title}
+                        courseId={params.courseId}
+                        nextChapterId={nextChapter?.id}
+                        playbackId={muxData?.playerId}
+                        isLocked={isLocked}
+                        completeOnEnd={completeOnEnd}
+                    />
+                </div>
+             </div>
         </div>
+       
      );
 }
  
