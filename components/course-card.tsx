37fcc8d8243cac9e095710dac80058dfbbb02e52
Novus-Tech237 <novustechnologies7@gmail.com>
2023-12-31 10:40:18 +0100

    Changes to be committed: 	modified:   app/(dashboard)/search/page.tsx 	modified:   components/course-card.tsx

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+import Image from "next/image";
+import { IconBadge } from "@/components/icon-badge";
+import { BookOpen } from "lucide-react";
+import { formatPrice } from "@/lib/format";
+
+
 interface CourseCardProps{
     id: string;
     title: string;
@@ -10,8 +17,35 @@ interface CourseCardProps{
 
 export const CourseCard = ({id, title, imageUrl, chaptersLength, price, progress, category}:CourseCardProps) => {
     return (
-        <div>
-            Course Card
+        <Link href={`/courses/${id}`}>
+      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+        <div className="relative w-full rounded-md overflow-hidden">
+          <Image
+            width={400}
+            height={400}
+            className="flex items-center justify-center"
+            alt={title}
+            src={imageUrl}
+          />
+          <div className="flex flex-col pt-2">
+            <div className="text-lg md:text-base font-medium group-hover:text-orange-700 line-clamp-2">
+                {title}
+            </div>
+            <p className="text-xs text-muted-foreground">
+                {category}
+            </p>
+            <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
+                <div className="flex items-center gap-x-1 text-slate-500">
+                    <IconBadge size="sm" icon={BookOpen}/>
+                    <span>{chaptersLength} {chaptersLength === 1? "Chapter":"Chapters"}</span>
+                </div>
+            </div>
+            {progress !== null ?(
+                <div>TODO: Progress Bar</div>
+            ):(<p className="text-sm md:text-sm font-medium text-slate-700">{formatPrice(price)}</p>)}
+          </div>
         </div>
+      </div>
+    </Link>
     )
 }
