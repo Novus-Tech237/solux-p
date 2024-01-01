74c85d20eb76ff150dc37eb861b14349c0f45fb2
Novus-Tech237 <novustechnologies7@gmail.com>
2024-01-01 10:01:27 +0100

    Changes to be committed: 	new file:   app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx 	modified:   app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx 	modified:   app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx 	new file:   app/api/courses/[courseId]/checkout/route.ts 	new file:   components/ui/separator.tsx 	new file:   lib/stripe.ts 	modified:   package-lock.json 	modified:   package.json

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -21,30 +21,31 @@ interface VideoPlayerProps{
 export const VideoPlayer =({playbackId, courseId, chapterId, nextChapterId, isLocked, completeOnEnd, title}: VideoPlayerProps) => {
     const [isReady, setIsReady] = useState(false)
     console.log(playbackId)
-    return(
+    return (
         <div className="relative aspect-video">
             {!isReady && !isLocked && (
                 <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
-                    <Loader2 className="h-8 w-8 animate-spin text-secondary"/>
+                    <Loader2 className="h-8 w-8 animate-spin text-secondary" />
                 </div>
             )}
             {isLocked && (
-                 <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
-                    <Lock className="h-8 w-8"/>
+                <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+                    <Lock className="h-8 w-8" />
                     <p className="text-sm">This Chapter is Locked</p>
                 </div>
             )}
-            {!isLocked &&(
-                <MuxPlayer 
-                    title={title}
-                    className={cn(!isReady && "hidden")}
-                    onCanPlay={()=>setIsReady(true)}
-                    onEnded={()=>{}}
-                    autoPlay
-                    playbackId={playbackId} 
-                />
+            {!isLocked && (
+                <div className="aspect-video">
+                    <MuxPlayer
+                        title={title}
+                        className={cn(!isReady && "hidden")}
+                        onCanPlay={() => setIsReady(true)}
+                        onEnded={() => {}}
+                        autoPlay
+                        playbackId={playbackId}
+                    />
+                </div>
             )}
-            
         </div>
     )
 }
