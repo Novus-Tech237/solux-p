4cfcd12ca0cdf1cf1b30f265aebb0f0de7febc14
Novus-Tech237 <novustechnologies7@gmail.com>
2024-01-03 17:46:22 +0100

    Changes to be committed: 	new file:   actions/get-analytics.ts 	new file:   actions/get-dashboard-courses.ts 	new file:   app/(dashboard)/(routes)/(root)/_components/info-card.tsx 	modified:   app/(dashboard)/(routes)/(root)/page.tsx 	new file:   app/(dashboard)/(routes)/teacher/analytics/_components/chart.tsx 	new file:   app/(dashboard)/(routes)/teacher/analytics/_components/data-card.tsx 	modified:   app/(dashboard)/(routes)/teacher/analytics/page.tsx 	new file:   components/ui/card.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,7 +1,46 @@
-import { UserButton } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+import { CheckCircle, Clock, Layout } from "lucide-react";
 
-export default function Home(){
-    return (
-        <div></div>
-    )
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+import { CoursesList } from "@/components/courses-list";
+
+import { InfoCard } from "./_components/info-card";
+
+export default async function Dashboard() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    completedCourses,
+    coursesInProgress
+  } = await getDashboardCourses(userId);
+
+  return (
+    <div className="p-6 space-y-4">
+        <div className="flex flex-row items-center gap-x-2 pt-5">
+            <Layout className="text-orange-600"/>
+            <h1 className="text-2xl font-bold text-orange-600">Dashboard</h1>
+        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+       <InfoCard
+          icon={Clock}
+          label="In Progress"
+          numberOfItems={coursesInProgress.length}
+       />
+       <InfoCard
+          icon={CheckCircle}
+          label="Completed"
+          numberOfItems={completedCourses.length}
+          variant="success"
+       />
+      </div>
+      <CoursesList
+        items={[...coursesInProgress, ...completedCourses]}
+      />
+    </div>
+  )
 }
--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,7 +1,47 @@
-const AnalyticsPage = () => {
-    return ( 
-        <div>Analytic Page</div>
-     );
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { getAnalytics } from "@/actions/get-analytics";
+
+import { DataCard } from "./_components/data-card";
+import { Chart } from "./_components/chart";
+import { BarChart, Compass } from "lucide-react";
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    data,
+    totalRevenue,
+    totalSales,
+  } = await getAnalytics(userId);
+
+  return ( 
+    <div className="p-6">
+        <div className="flex flex-row items-center gap-x-2 pt-5 pb-5">
+            <BarChart className="text-orange-600"/>
+            <h1 className="text-2xl font-bold text-orange-600">Analytics</h1>
+        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard
+          label="Total Revenue"
+          value={totalRevenue}
+          shouldFormat
+        />
+        <DataCard
+          label="Total Sales"
+          value={totalSales}
+        />
+      </div>
+      <Chart
+        data={data}
+      />
+    </div>
+   );
 }
  
 export default AnalyticsPage;
